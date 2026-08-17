88b6260039be55367d1f5491d7706787454a7833
Fauzi Gilang Raihan <fauzigilangr10@gmail.com>
2026-08-17 21:21:19 +0700

    uppdate penyesuian

--- app/page.tsx
+++ app/page.tsx
@@ -321,7 +321,7 @@ function ChatInterface() {
   };
 
   return (
-    <div className="flex h-screen w-full overflow-hidden bg-zinc-50 dark:bg-[#09090b]">
+    <div className="flex h-screen h-[100dvh] w-full overflow-hidden bg-zinc-50 dark:bg-[#09090b]">
       {/* Sidebar */}
       <Sidebar
         isOpen={isSidebarOpen}
